cee2a571848f6fa5c231d8328f420a6cc6476fbf
Bela Ban <belaban@yahoo.com>
2016-12-26 14:27:07 +0100

    ns

---
+++ src/org/test/Test.java
@@ -0,0 +1,14 @@
+import rx.Observable;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Observable<String> obs=Observable.create(s -> {
+            s.onNext("hello world");
+            s.onNext("last one");
+            s.onCompleted();
+        });
+        obs.subscribe(action -> System.out.printf("action: %s\n", action));
+    }
+}
